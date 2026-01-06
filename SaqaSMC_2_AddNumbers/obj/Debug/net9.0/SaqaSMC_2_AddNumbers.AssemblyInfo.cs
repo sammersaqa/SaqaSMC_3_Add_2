b14371158f159c0e673c02d006975cf1db6995d5
sammersaqa <sammersaqa@gmail.com>
2026-01-06 14:35:44 -0800

    no majors

--- SaqaSMC_2_AddNumbers/obj/Debug/net9.0/SaqaSMC_2_AddNumbers.AssemblyInfo.cs
+++ SaqaSMC_2_AddNumbers/obj/Debug/net9.0/SaqaSMC_2_AddNumbers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaqaSMC_2_AddNumbers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd281c00b2cdbb489899d66bd986a76f02c08de8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4619e3e103389afe100b74265a03961a089b5ee0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaqaSMC_2_AddNumbers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaqaSMC_2_AddNumbers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
